6f7cc71634cf6b9313d12312c2209d6e3753017f
mehalanis29 <47949751+mehalanis29@users.noreply.github.com>
2019-03-26 16:10:49 +0100

    index client

--- indexclient.php
+++ indexclient.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php
 include 'php/Client/standard.php';
+
  ?>
 <html lang="en" dir="ltr">
   <head>
@@ -10,6 +11,39 @@ include 'php/Client/standard.php';
     <link rel="stylesheet" href="css/Client/navbar.css">
   </head>
   <body>
-    <?php NabBar(); ?>
+    <div class="nav_bar">
+        <div class="nav_bar_head">
+          <div class="nav_bar_div_logo">
+            <label class="nav_bar_logo">Logo</label>
+          </div>
+          <div class="nav_bar_list_btn">
+            <a href="#" class="nav_bar_btn">Home</a>
+            <a href="#" class="nav_bar_btn">Voyage Organise</a>
+          </div>
+        </div>
+        <div class="nav_bar_cover_index">
+          <div class="nav_bar_div_form">
+            <form class="formtest" action="indexclient.php" method="post">
+              <div class="nab_bar_index_div_input">
+                <select class="nab_bar_index_input" placeholder="pays" name="">
+                  <option value="">Pays</option>
+                </select>
+              </div>
+              <div class="nab_bar_index_div_input">
+                <select class="nab_bar_index_input" placeholder="ville" name="">
+                  <option value="">ville</option>
+                </select>
+              </div>
+              <div class="nab_bar_index_div_input">
+                <label class="nab_bar_index_label"> </label>
+                <input type="date" class="nab_bar_index_input" placeholder="à partir" name="" value="">
+              </div>
+              <div class="nab_bar_index_div_input">
+                <button type="submit" class="recharche_btn" name="Recharche">Recharche</button>
+              </div>
+            </form>
+          </div>
+        </div>
+    </div>
   </body>
 </html>
